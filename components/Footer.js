06a75a4966649ed45ffd10b82f998545b469115b
Christian Genco <christian@gen.co>
2023-04-13 14:13:14 -0500

    add the footer back but titlecased this time

---
+++ components/Footer.js
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <footer className="">
+      Made by{" "}
+      <a
+        className="hover:underline text-blue-500"
+        href="https://christian.gen.co"
+      >
+        Christian Genco
+      </a>
+      .{" "}
+      <a
+        className="hover:underline text-blue-500"
+        href="https://twitter.com/cgenco"
+      >
+        @cgenco
+      </a>
+    </footer>
+  );
+}
